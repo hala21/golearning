550f2da3c4fe7196df5a4e3f2b8f9665bc3ab127
hala21 <lizhiyong567@gmail.com>
2019-05-20 22:16:07 +0800

    judgment when to run firewall command.

--- scripts/dyncip.go
+++ scripts/dyncip.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"database/sql"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -58,19 +60,61 @@ func main() {
 	if err != nil {
 		logger.Fatal(err)
 	}
+
+	// 假如ip.txt为空，需要更新文件，并且判断是否有文件
 	if result == nil {
+		//空文件的时候新建ip.txt文件
 		ioutil.WriteFile(stockIpFile, []byte(stockIP), 0777)
+		// 判断数据库和防火墙的IP是否相同，不同的话，需要修改防火墙IP地址
+		sshClient, err := sshConn(ssh_username, ssh_password, ssh_server, ssh_port)
+		if err != nil {
+			logger.Fatal("连接防火墙失败")
+		}
+		session, err := sshClient.NewSession()
+		if err != nil {
+			logger.Fatal("不能创建防火墙ssh session")
+		}
+		defer session.Close()
+		var sessionErr, out bytes.Buffer
+		session.Stdout = &out
+		session.Stderr = &sessionErr
+		session.Run("show configuration security zones security-zone untrust address-book address xiyanghzc")
+		logger.Fatal((sessionErr).String())
+		ip := out.String()
+		// 比较IP字符串
+		if !strings.Contains(ip, stockIP) {
+			session.Run("configure")
+			session.Run("set security zones security-zone untrust address-book address xiyanghzc " + stockIP + "/32")
+			session.Run("commit and-quit")
+			session.Run("exit")
+			session.Wait()
+			logger.Fatal((sessionErr).String())
+		}
+
 	} else {
+		// 数据库和文件IP不匹配的时候
 		if stockIP != string(result) {
 			sshClient, err := sshConn(ssh_username, ssh_password, ssh_server, ssh_port)
+			if err != nil {
+				logger.Fatal("连接防火墙失败")
+			}
 			session, err := sshClient.NewSession()
 			if err != nil {
-				logger.Fatal("can not creat  session")
+				logger.Fatal("不能创建防火墙ssh session")
 			}
-			session.Run()
+			defer session.Close()
 
-			session.Run()
+			// 防火墙配置
+			var sessionErr, out bytes.Buffer
+			session.Stdout = &out
+			session.Stderr = &sessionErr
 
+			session.Run("configure")
+			session.Run("set security zones security-zone untrust address-book address xiyanghzc " + stockIP + "/32")
+			session.Run("commit and-quit")
+			session.Run("exit")
+			session.Wait()
+			logger.Fatal((sessionErr).String())
 		}
 	}
 
